9f976243bc02d07cdc5dd3999eb2564dc41a2e10
xerega <xerega.dev@gmail.com>
2024-06-03 07:50:34 +0300

    fix: change nest port

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(8080);
+  await app.listen(8081);
 }
 bootstrap();
